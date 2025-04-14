18dee298ce625f26552f86c42c04cdff5c8a1299
dantoniazzo <85890036+dantoniazzo@users.noreply.github.com>
2025-04-14 10:32:41 +0200

    fix: remove touch native mobile events

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toolbar, Canvas } from 'widgets';
 
 function App() {
   return (
-    <div className="h-screen w-screen bg-black">
+    <div className="h-screen w-screen bg-black touch-none">
       <Canvas />
       <Toolbar />
     </div>
